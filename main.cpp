2c84d946911ae4a406760dc456d51bfe0b786003
Stockmite <rojogreenazul2@gmail.com>
2026-03-12 18:08:23 -0300

    Oh, how did it not occur to me? My function for filtering out characters was also going to filter out special characters! And here I was immediately trying to come up with a solution rather than trying to first patiently diagnose the problem.
    Well, I guess we do still have a problem. The program now crashes, instanly

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include <winuser.h>
 #include <windef.h>
 
-#define IsPressed 0x8000
 #define shiftcode1 0x10
 #define shiftcode2 0xA0
+#define IsPressed 0x8000
 
 using namespace std;
 
 bool DoesValGiveChar(int KeyCode) {
-    return 48 <= KeyCode && KeyCode <= 93;
+    return 0 <= KeyCode && KeyCode <= 255;
 }
 
 int main() {
@@ -19,7 +19,7 @@ int main() {
     char Word[21];
     int ind = 0;
 
-    bool PressedKeys[128] = {false};
+    bool PressedKeys[255] = {false};
 
     while (ind < 100) {
         bool MakeUppercase = GetAsyncKeyState(shiftcode1) & GetAsyncKeyState(shiftcode2) & IsPressed;
@@ -28,16 +28,17 @@ int main() {
         //-Terry Davis
         for (int KeyCode = 0; KeyCode < 255; KeyCode++) {
             if (KeyCode==shiftcode1 || KeyCode==shiftcode2) {continue;}
-            if ((GetAsyncKeyState(KeyCode) & IsPressed)) {
+            if ((GetKeyState(KeyCode))) {
                 if (DoesValGiveChar(KeyCode)) {
                     int charIndex = KeyCode - 48;
 
                     if (!PressedKeys[charIndex]) {
-                        char SelectedChar = (char)(KeyCode);
+                        char SelectedChar = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
                         char PressedKey = (MakeUppercase) ? SelectedChar : tolower(SelectedChar);
+                        SelectedChar &= IsPressed;
 
                         PressedKeys[charIndex] = true;
-                        printf("%c", PressedKey);
+                        printf("%c", SelectedChar);
                         ind++;
                     }
                 }
